d58daf668c7b45cce846ce1d48aaa39f0f050fac
readmyface <sarthak.chakraborty0@gmail.com>
2020-04-23 00:52:59 +0530

    First Commit 23/04

---
+++ precache-manifest.612f71ff50adfc754b9b959204a58ed3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "694f541f257531a188ba",
+    "url": "/static/js/main.92266e43.chunk.js"
+  },
+  {
+    "revision": "d8a7a81568ff85581be0",
+    "url": "/static/js/2.3ae5c7bd.chunk.js"
+  },
+  {
+    "revision": "694f541f257531a188ba",
+    "url": "/static/css/main.d9f4cba2.chunk.css"
+  },
+  {
+    "revision": "84af1295189fa23b027323268f730b10",
+    "url": "/index.html"
+  }
+];
